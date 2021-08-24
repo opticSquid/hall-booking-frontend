42dc31aebcaef5a5ee6d91805d0eb05ff6079aa5
Soumalya Bhattacharya <59192460+soumalyatheking22012001@users.noreply.github.com>
2021-08-24 19:35:35 +0530

    Login Page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './material/material.module';
 
 import { AppComponent } from './app.component';
@@ -11,11 +12,18 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HeroImageComponent, LoginComponent, SignupComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeroImageComponent,
+    LoginComponent,
+    SignupComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MaterialModule,
   ],
   providers: [],
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,12 +4,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 const materialModules = [
   MatToolbarModule,
   MatIconModule,
   MatGridListModule,
   MatButtonModule,
   MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
 ];
 
 @NgModule({
